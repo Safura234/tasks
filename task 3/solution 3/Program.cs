7b97907252c538cba175d1537d084f9099fbf1f2
Safura234 <safuramg@code.edu.az>
2022-03-01 16:12:11 +0400

    3 reqemli eded verilib. Bu ededin axirina hemin ededdin ozunu yapishdir. example: 333= 333333;

---
+++ task 3/solution 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace solution_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 321;
+            int c = a * 1000 + b;
+            Console.WriteLine(c);
+        }
+    }
+}
