625f6405e63f362ed5a08c8fc74c406779b5c009
Safura234 <safuramg@code.edu.az>
2022-03-02 19:17:08 +0400

    5 reqemli eded verilib. Bu ededin evvel 18 % sonra ise 3% tap

---
+++ solution4/solution4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace solution4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 54000;
+            int b = 18;
+            int c = a * 100 / b;
+            Console.WriteLine(c);
+            int d = 3;
+            int e = c * 100 / d;
+            Console.WriteLine(e);
+
+
+        }
+    }
+}
