972a4c40b0bb61539ee6cc2568a7cca8ee2e5c86
Safura234 <safuramg@code.edu.az>
2022-03-03 16:52:12 +0400

    3 reqemli eded verilib. Bu ededin axirina 7 artir. Sonra cavabin 7% tap;

---
+++ task5/task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 210;
+            int b = 7;
+            int c = a * 10 + 7;
+            Console.WriteLine(c);
+            int d = c * 100 / b;
+            Console.WriteLine(d);
+        }
+    }
+}
