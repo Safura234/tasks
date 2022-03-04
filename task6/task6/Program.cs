004d1b5a3230a3904de7da452591674dc33dc55a
Safura234 <safuramg@code.edu.az>
2022-03-04 13:51:21 +0400

    4 reqemli eded verilib. Bu ededin evvel 20%-ni , sonra ise cavabin 10% tap. Alinan cavabin kvadratini tap.

---
+++ task6/task6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1234;
+            if (a > 1000 || a <= 10000) ;
+            Console.WriteLine("4reqemli");
+            int b = 20;
+            int c = a * 100 / 20;
+            Console.WriteLine(c);
+            int d = c * 100 / 10;
+            Console.WriteLine(d);
+            int e = d * d;
+            Console.WriteLine(e);
+
+
+
+        }
+    }
+}
