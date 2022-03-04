ce03fdd04c40d0347042b3f788e5fb0181cfdb92
Safura234 <safuramg@code.edu.az>
2022-03-04 15:11:17 +0400

    2 dene 5 reqemli eded verilib. Bu ededleri toplayib. Alinan cavabin evveline ve axirina 5 artir. Neticenin 5 % tap

---
+++ task7/task7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12340;
+            int b = 23560;
+            if (a < 10000 || a >= 100000);
+            if (b < 10000 || b >= 100000) ;
+            Console.WriteLine("5reqemlidir");
+            int c = a + b;
+            Console.WriteLine(c);
+            int d = 500000;
+            int e = 5;
+            int f = (d + c) * 10 + 5;
+            Console.WriteLine(f);
+            int g = f * 100 / e;
+            Console.WriteLine(g);
+
+
+        }
+    }
+}
