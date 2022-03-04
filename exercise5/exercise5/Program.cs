21ecaa126806a7ee49b8b933a52e0ad1f7ec4494
Safura234 <safuramg@code.edu.az>
2022-03-04 13:13:33 +0400

    tasklarda 5ci tapshirig

---
+++ exercise5/exercise5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4444;
+            if (a < 1000 || a >= 10000);
+            Console.WriteLine ("4reqemlidir");
+            int b = 40000;
+            int c = a + b;
+            Console.WriteLine(c);
+            int d = 44;
+            int e = c * 100 + d;
+            Console.WriteLine(e);
+            int f = e * 100 / d;
+            Console.WriteLine(f);
+
+
+
+
+        }
+    }
+}
