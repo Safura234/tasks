e9e7402c559d1105b6e736abcbab20eed150609b
Safura234 <safuramg@code.edu.az>
2022-03-05 09:58:36 +0400

    2 dene eded verilib. I eded 4 reqemli II eded 7 reqemlidir. I ededin 4%-ni tap. Sonra II ededin 9% ni tap. Sonra Cavalari toplayib 10 %ni tap.

---
+++ Solution8/solution8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace solution8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1234;
+            if (a < 1000 || a >= 10000) ;
+            Console.WriteLine("4reqemlidir");
+            int b = 4;
+            int c = 1234 * 100 / 4;
+            Console.WriteLine(c);
+            int d = 5678900;
+            if (a < 1000000 || a >= 10000000) ;
+            Console.WriteLine("7reqemlidir");
+            int e = 9;
+            int f = 5678900 * 100 / 9;
+            Console.WriteLine(f);
+            int g = c + f;
+            Console.WriteLine(g);
+            int h = 10;
+            int i = g * 100 / 10;
+            Console.WriteLine(i);
+
+
+        }
+    }
+}
