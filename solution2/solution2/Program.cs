e31b39bb266216594d7e41afe2cfb4570f2163e4
Safura234 <safuramg@code.edu.az>
2022-03-07 18:03:17 +0400

    task 1in pseudo code

---
+++ solution2/solution2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace solution2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12345;
+            int b = 23450;
+            int c = 500000;
+            int d = 5;
+            if (a < 10000 || a >= 100000) ;
+            if (b < 10000 || b >= 100000) ;
+            Console.WriteLine("5 reqemlidir");
+            int e = a + b;
+            Console.WriteLine(e);
+            int f = (e + c) * 10 + 5;
+            Console.WriteLine(f);
+            
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
